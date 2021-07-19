89a62399cfaf2ebee1c96ab206ebdc16f811503e
Efren <romerzu@hotmail.com>
2021-07-19 17:12:54 -0400

    update admin

--- survey/admin.py
+++ survey/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from survey.models import Question, Answer
+from survey.models import Question, Answer, QuestionLike
 
 class QuestionAdmin(admin.ModelAdmin):
     pass
@@ -7,5 +7,9 @@ class QuestionAdmin(admin.ModelAdmin):
 class AnswerAdmin(admin.ModelAdmin):
     pass
 
+class QuestionLikeAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer, AnswerAdmin)
+admin.site.register(QuestionLike, QuestionLikeAdmin)
