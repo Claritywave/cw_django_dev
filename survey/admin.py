0a1687fc4114d9587f57d61200f6dee53071f97b
Efren <romerzu@hotmail.com>
2021-07-19 14:19:27 -0400

    admin models added for survey

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from survey.models import Question, Answer
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
